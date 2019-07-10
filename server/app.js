6fba5504beeba1cd36a3da87bdfbe2cfdd33eba5
Mike Murray <MikeMurrayDev@gmail.com>
2019-07-10 15:13:10 -0700

    Adds server logic to push json data to the front end; listens on port 3000.

--- server/app.js
+++ server/app.js
@@ -1,10 +1,27 @@
 // import files and packages up here
+const express = require('express');
+const morgan = require('morgan');
+const data = require('./data');
 
+const PORT = process.env.PORT || 3000;
 
 // create your express server below
-var app;
+var app = express();
 
 // add your routes and middleware below
+app.use(morgan('dev'));
+
+app.get('/', function(req, res) {
+    res.status(200).send('New Express server');
+})
+
+app.get('/data', function(req, res){
+    res.status(200).send(data);
+})
+
+app.get('*', function(req, res){
+    res.status(404).send('No Page Found');
+})
 
 // finally export the express application
 module.exports = app;
